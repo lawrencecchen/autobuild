98e837eee928a8d85fc5e25ce0fe98875793c573
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2024-03-04 02:16:44 -0800

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,6 @@ export const metadata: Metadata = {
     default: "Autobuild",
     template: `%s - Autobuild`,
   },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
   twitter: {
     ...meta,
     card: "summary_large_image",
@@ -33,6 +28,87 @@ export const metadata: Metadata = {
     ...meta,
     locale: "en-US",
     type: "website",
+  },
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "57x57",
+      url: "/apple-icon-57x57.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "60x60",
+      url: "/apple-icon-60x60.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "72x72",
+      url: "/apple-icon-72x72.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "76x76",
+      url: "/apple-icon-76x76.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "114x114",
+      url: "/apple-icon-114x114.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "120x120",
+      url: "/apple-icon-120x120.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "144x144",
+      url: "/apple-icon-144x144.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      url: "/apple-icon-152x152.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/apple-icon-180x180.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/android-icon-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      url: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "manifest",
+      url: "/manifest.json",
+    },
+  ],
+  other: {
+    "msapplication-TileColor": "#ffffff",
+    "msapplication-TileImage": "/ms-icon-144x144.png",
+    "theme-color": "#ffffff",
   },
 };
 
