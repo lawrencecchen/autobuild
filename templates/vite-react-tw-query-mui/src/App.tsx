518fbbff2e81700431a55005a97bfb0a40c6073b
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2024-03-04 02:20:47 -0800

    wip

--- templates/vite-react-tw-query-mui/src/App.tsx
+++ templates/vite-react-tw-query-mui/src/App.tsx
@@ -5,38 +5,38 @@ const searchParams = new URLSearchParams(window.location.search);
 const componentName = searchParams.get("c");
 
 function Comment() {
-	return (
-		<>
-			<Highlighter />
-		</>
-	);
+  return (
+    <>
+      <Highlighter />
+    </>
+  );
 }
 
 // Dynamically import the component based on the component name
 const UserComponent = componentName
-	? React.lazy(() => import(`./c/${componentName}.tsx`))
-	: null;
+  ? React.lazy(() => import(`./c/${componentName}.tsx`))
+  : null;
 
 function App() {
-	const iframeReady = useRef(false);
-	useEffect(() => {
-		if (iframeReady.current) {
-			return;
-		}
-		iframeReady.current = true;
-		window.postMessage("iframe-ready", "*");
-	}, []);
-	if (!componentName) {
-		return <div className="p-4">No component</div>;
-	}
-	return (
-		<>
-			<Suspense fallback={<div>Loading...</div>}>
-				{UserComponent && <UserComponent />}
-			</Suspense>
-			<Comment />
-		</>
-	);
+  const iframeReady = useRef(false);
+  useEffect(() => {
+    if (iframeReady.current) {
+      return;
+    }
+    iframeReady.current = true;
+    window.postMessage("iframe-ready", "*");
+  }, []);
+  if (!componentName) {
+    return <div className="p-4">No component</div>;
+  }
+  return (
+    <>
+      <Suspense fallback={<div className="p-4">Loading...</div>}>
+        {UserComponent && <UserComponent />}
+      </Suspense>
+      <Comment />
+    </>
+  );
 }
 
 export default App;
